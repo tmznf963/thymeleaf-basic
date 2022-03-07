92cb7684ab1688e98f458486ec1adb981237bf98
MINO <qhfhd963@naver.com>
2022-03-07 17:32:09 +0900

    thymeleaf basic

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.Map;
 @RequestMapping("/basic")
 public class BasicController {
 
+    //타임리프 문법 "텍스트, utext"
     @GetMapping("/text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello Spring!");
@@ -27,6 +31,7 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+    //타임리프 문법 "변수"
     @GetMapping("/variable")
     public String variable(Model model) {
         User userA = new User("userA", 10);
@@ -56,5 +61,49 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObejects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    //helloBean 이라는 이름의 SpringBean 생성
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello " + data;
+        }
+    }
+
+    //자바8 날짜용 유틸리티 객체
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    //타임리프 문법 "URL 링크"
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    //타임리프 문법 "리터럴 Literals"
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    //타임리프 문법 "연산"
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
     }
 }
